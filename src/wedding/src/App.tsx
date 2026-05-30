ec944cff8083d5033ce5d305dbda75792652cb02
WHDesigns <wyatt.haak4@gmail.com>
2026-05-29 19:00:52 -0700

    made the website again

---
+++ src/wedding/src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">Contained</Button>
+    </>
+  );
+}
+
+export default App;
