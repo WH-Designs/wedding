cea510a87ea0153ec6961ca82594be0a82166570
WHDesigns <wyatt.haak4@gmail.com>
2026-05-29 19:07:29 -0700

    adjusted the build

--- src/wedding/vite.config.ts
+++ src/wedding/vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wedding/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
